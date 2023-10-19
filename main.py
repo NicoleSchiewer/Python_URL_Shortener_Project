b4fc7434fc868d55241172db13cba9c84c733763
Nicole Schiewer <nschiewer@catalyte,io>
2023-10-19 16:06:15 -0500

    Add code to create a shortened url from a provided url

--- main.py
+++ main.py
@@ -4,17 +4,12 @@ import validators
 from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
 
+import schemas 
+import models
 from database import SessionLocal, engine
 
-from models import URL, Base
-from schemas import URLBase, URLInfo
-
-def raise_bad_request(message):
-    raise HTTPException(status_code=400, detail=message)
-
 app = FastAPI()
-
-Base.metadata.create_all(bind=engine)
+models.Base.metadata.create_all(bind=engine)
 
 def get_db():
     db = SessionLocal()
@@ -23,8 +18,21 @@ def get_db():
     finally:
         db.close()
 
-@app.post("/url", response_model=URLInfo)
-def create_url(url: URLBase, db: Session = Depends(get_db)):
+def raise_bad_request(message):
+    raise HTTPException(status_code=400, detail=message)
+
+@app.get("/")
+def read_root():
+    return "Welcome to the URL shortener API :)"
+
+#@app.post("/url")
+#def create_url(url: schemas.URLBase):
+ #   if not validators.url(url.target_url):
+  #      raise_bad_request(message="Your provided URL is not valid")
+   # return f"TODO: Create database entry for: {url.target_url}"
+
+@app.post("/url", response_model=schemas.URLInfo)
+def create_url(url: schemas.URLBase, db: Session = Depends(get_db)):
     if not validators.url(url.target_url):
         raise_bad_request(message="Your provided URL is not valid")
 
@@ -40,4 +48,5 @@ def create_url(url: URLBase, db: Session = Depends(get_db)):
     db_url.url = key
     db_url.admin_url = secret_key
 
+    return db_url
     return db_url
